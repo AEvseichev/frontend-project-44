8c70da6d23ecba094841551032d56a6af677c407
AEvseichev <andrey.goltman@gmail.com>
2026-03-29 18:33:41 +0300

    create progression.js

---
+++ src/games/progression.js
@@ -0,0 +1,10 @@
+import crypto from 'crypto'
+import startEngine from '../index.js'
+
+const description = 'What number is missing in the progression?'
+
+const startRound = () => {
+  
+}
+
+export default () => startEngine(description, startRound)
