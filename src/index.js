e9e3762da897d7ab8ef7fb8a0ff57ebd64864f5b
AEvseichev <andrey.goltman@gmail.com>
2026-03-29 14:08:51 +0300

    create calc and index.js

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import readLineSync from 'readline-sync';
+
+
+function startEngine(description, startRound) => {
+    console.log('Welcome to the Brain Games!')
+    const userName = readLineSync.question('May I have your name?')
+    console.log(`Hello, ${userName}!`)
+
+    console.log(description)
+
+    const countTrueAnswer = 3
+
+
+
+
+
+}
